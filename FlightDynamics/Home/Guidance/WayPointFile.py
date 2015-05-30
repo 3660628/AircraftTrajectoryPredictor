5530b88023133df6435717e0604cdc3255b98bde
Robert <robert.pastor0691@orange.fr>
2015-05-30 14:43:29 +0200

    added test to WayPoint class
    added test to WayPoint class

--- FlightDynamics/Home/Guidance/WayPointFile.py
+++ FlightDynamics/Home/Guidance/WayPointFile.py
@@ -28,6 +28,7 @@ Created on 13 juil. 2014
 from Home.Guidance.Haversine import points2distanceMeters, points2bearingDegrees, LatitudeLongitudeAtDistanceBearing
 import time
 import math
+import unittest
 
 def to_positive_angle(angleDegrees):
     angleDegrees = math.fmod(angleDegrees, 360);
@@ -192,41 +193,48 @@ class Airport(WayPoint):
         WayPoint.dump(self)
         print "airport field Elevation above Sea Level Meters=",self.fieldElevationAboveSeaLevelMeters, " meters"
         print 'airport ICAO code= ' + self.ICAOcode
+
+
+
+class Test_WayPoint(unittest.TestCase):
+
+
+    def test_WayPoint(self):
+    
+        print "=========== WayPoint start  =========== " + time.strftime("%c")
+        London = WayPoint('London-Heathrow', 51.5, 0.0)
+        Orly = WayPoint('Orly', 48.726254, 2.365247)
+        print "distance from London to Orly= ", London.getDistanceMetersTo(Orly), " meters"
+        print "bearing from London to Orly= ", London.getBearingDegreesTo(Orly), " degrees"
         
-#============================================
+        #Zurich = WayPoint('Zurich-Kloten', 47.458215, 8.555424)
+        Marseille = WayPoint('Marseille-Marignane', 43.438431, 5.214382 )
+        Zurich = WayPoint('Zurich-Kloten', 47.458215, 8.555424)
+        
+        print "=========== WayPoint resume  =========== " + time.strftime("%c")
+    
+        print "distance from Marseille to Zurich= ", Marseille.getDistanceMetersTo(Zurich), " meters"
+        print "bearing from Zurich to Marseille = ", Zurich.getBearingDegreesTo(Marseille), " degrees"
+        
+        distanceMeters = 321584.699454
+        bearingDegrees = Zurich.getBearingDegreesTo(Marseille)
+        #bearingDegrees = Marseille.getBearingDegreesTo(Zurich)
+        Zurich.dump()
+        Marseille.dump()
+        TopOfDescent = Zurich.getWayPointAtDistanceBearing('TopOfDescent', distanceMeters, bearingDegrees)
+        TopOfDescent.dump()
+        
+        print "=========== WayPoint resume  =========== " + time.strftime("%c")
+        London.dump()
+        Orly.dump()
+        bearingDegrees = Orly.getBearingDegreesTo(London)
+        print "bearing from London to Orly= ", London.getBearingDegreesTo(Orly), " degrees"
+    
+        TopOfDescent = Orly.getWayPointAtDistanceBearing('TopOfDescent', distanceMeters, bearingDegrees)
+        TopOfDescent.dump()
+        
+    
+
 if __name__ == '__main__':
+    unittest.main()
     
-    print "=========== WayPoint start  =========== " + time.strftime("%c")
-    London = WayPoint('London-Heathrow', 51.5, 0.0)
-    Orly = WayPoint('Orly', 48.726254, 2.365247)
-    print "distance from London to Orly= ", London.getDistanceMetersTo(Orly), " meters"
-    print "bearing from London to Orly= ", London.getBearingDegreesTo(Orly), " degrees"
-    
-    #Zurich = WayPoint('Zurich-Kloten', 47.458215, 8.555424)
-    Marseille = WayPoint('Marseille-Marignane', 43.438431, 5.214382 )
-    Zurich = WayPoint('Zurich-Kloten', 47.458215, 8.555424)
-    
-    print "=========== WayPoint resume  =========== " + time.strftime("%c")
-
-    print "distance from Marseille to Zurich= ", Marseille.getDistanceMetersTo(Zurich), " meters"
-    print "bearing from Zurich to Marseille = ", Zurich.getBearingDegreesTo(Marseille), " degrees"
-    
-    distanceMeters = 321584.699454
-    bearingDegrees = Zurich.getBearingDegreesTo(Marseille)
-    #bearingDegrees = Marseille.getBearingDegreesTo(Zurich)
-    Zurich.dump()
-    Marseille.dump()
-    TopOfDescent = Zurich.getWayPointAtDistanceBearing('TopOfDescent', distanceMeters, bearingDegrees)
-    TopOfDescent.dump()
-    
-    print "=========== WayPoint resume  =========== " + time.strftime("%c")
-    London.dump()
-    Orly.dump()
-    bearingDegrees = Orly.getBearingDegreesTo(London)
-    print "bearing from London to Orly= ", London.getBearingDegreesTo(Orly), " degrees"
-
-    TopOfDescent = Orly.getWayPointAtDistanceBearing('TopOfDescent', distanceMeters, bearingDegrees)
-    TopOfDescent.dump()
-    
-
-        
