25d6ae886373c73998aa645434aa61e86d567d90
Robert <robert.pastor0691@orange.fr>
2015-05-31 20:34:41 +0200

    add test in Base Turn Leg
    add test in Base Turn Leg

--- FlightDynamics/Home/Guidance/BaseTurnLegFile.py
+++ FlightDynamics/Home/Guidance/BaseTurnLegFile.py
@@ -30,6 +30,7 @@ is used in case of SIMULATED arrival turn.
 
 '''
 import time
+import unittest
 
 class BaseTurnLeg(object):
     
@@ -124,31 +125,40 @@ class BaseTurnLeg(object):
     
     
 #============================================
+class Test_Class(unittest.TestCase):
+
+    def test_Class(self):
+
+    
+        print "=========== Base Turn Leg testing   =========== " + time.strftime("%c")
+        
+        baseTurnLeg = BaseTurnLeg(150.0, 190.0, 1.0)
+        baseTurnLeg.build()
+        print baseTurnLeg
+        
+        print "=========== Base Turn Leg testing   =========== " + time.strftime("%c")
+    
+        baseTurnLeg = BaseTurnLeg(350.0, 10.0, 1.0)
+        baseTurnLeg.build()
+        print baseTurnLeg
+        
+        print "=========== Base Turn Leg testing   =========== " + time.strftime("%c")
+        baseTurnLeg = BaseTurnLeg(10.0, 350.0, -1.0)
+        baseTurnLeg.build()
+        print baseTurnLeg
+        
+        print "=========== Base Turn Leg testing   =========== " + time.strftime("%c")
+        baseTurnLeg = BaseTurnLeg(270.0, 80.0, -1.0)
+        baseTurnLeg.build()
+        print baseTurnLeg
+        
+        print "=========== Base Turn Leg testing   =========== " + time.strftime("%c")
+        try:
+            BaseTurnLeg(361.0, 0.0, 0.0)
+            self.assertFalse(True)
+        except:
+            self.assertTrue(True)
+
+
 if __name__ == '__main__':
-    
-
-    
-    print "=========== Base Turn Leg testing   =========== " + time.strftime("%c")
-    
-    baseTurnLeg = BaseTurnLeg(150.0, 190.0, 1.0)
-    baseTurnLeg.build()
-    print baseTurnLeg
-    
-    print "=========== Base Turn Leg testing   =========== " + time.strftime("%c")
-
-    baseTurnLeg = BaseTurnLeg(350.0, 10.0, 1.0)
-    baseTurnLeg.build()
-    print baseTurnLeg
-    
-    print "=========== Base Turn Leg testing   =========== " + time.strftime("%c")
-    baseTurnLeg = BaseTurnLeg(10.0, 350.0, -1.0)
-    baseTurnLeg.build()
-    print baseTurnLeg
-    
-    print "=========== Base Turn Leg testing   =========== " + time.strftime("%c")
-    baseTurnLeg = BaseTurnLeg(270.0, 80.0, -1.0)
-    baseTurnLeg.build()
-    print baseTurnLeg
-    
-    print "=========== Base Turn Leg testing   =========== " + time.strftime("%c")
-    errorOne = BaseTurnLeg(361.0, 0.0, 0.0)
+    unittest.main()
