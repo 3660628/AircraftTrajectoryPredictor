f59ab8b90364ea11bb4d784f769b1521683e2822
Robert <robert.pastor0691@orange.fr>
2015-06-01 10:17:59 +0200

    Allow test to be run from HOME
    Allow test to be run from HOME

--- FlightDynamics/Home/xlrd/tests/test_xlrd.py
+++ FlightDynamics/Home/xlrd/tests/test_xlrd.py
@@ -3,30 +3,46 @@ Created on Mar 31, 2015
 
 @author: t0007330
 '''
+import unittest
+import os
 from Home.xlrd import open_workbook
 
-if __name__ == "__main__":
+class Test_xlrd(unittest.TestCase):
 
-    WayPointsDict = {}
-    ColumnNames = []
-    
-    fileName = 'WayPoints.xls'
-    book = open_workbook(fileName)
-    sheet = book.sheet_by_index(0)
-    for row in range(sheet.nrows): 
-        rowValues = sheet.row_values(row, start_colx=0, end_colx=sheet.ncols)
-        # Print the values of the row formatted to 10 characters wide
-        if row == 0:
-            ColumnNames = list(rowValues)
-            print ColumnNames
+    def test_xlrd(self):
+
+        WayPointsDict = {}
+        ColumnNames = []
+        
+        fileName = 'WayPoints.xls'
+        if ('FlightDynamics' in os.getcwd()) and not('Home' in os.getcwd()):
+            fileName = os.getcwd() + os.path.sep + 'Home' + os.path.sep + 'xlrd' + os.path.sep + 'tests' + os.path.sep + fileName
         else:
-            if rowValues[0] in WayPointsDict.keys():
-                raise ValueError ('key- {0} already existing'.format(rowValues[0]))
-            WayPointsDict[rowValues[0]] = rowValues
-            
-        print  tuple(rowValues)
-    print '==================='
-    print ColumnNames
-    print '==================='
-    for key in WayPointsDict.keys():
-        print key, WayPointsDict[key]
+            ''' case when the run is launched from Home '''
+            fileName = os.getcwd() + os.path.sep + fileName
+
+        print 'file name= {0}'.format(fileName)
+        book = open_workbook(fileName)
+        sheet = book.sheet_by_index(0)
+        for row in range(sheet.nrows): 
+            rowValues = sheet.row_values(row, start_colx=0, end_colx=sheet.ncols)
+            # Print the values of the row formatted to 10 characters wide
+            if row == 0:
+                ColumnNames = list(rowValues)
+                print ColumnNames
+            else:
+                if rowValues[0] in WayPointsDict.keys():
+                    raise ValueError ('key- {0} already existing'.format(rowValues[0]))
+                WayPointsDict[rowValues[0]] = rowValues
+                
+            print  tuple(rowValues)
+        print '==================='
+        print ColumnNames
+        print '==================='
+        self.assertTrue(len(WayPointsDict)>0)
+        for key in WayPointsDict.keys():
+            print key, WayPointsDict[key]
+
+
+if __name__ == '__main__':
+    unittest.main()
