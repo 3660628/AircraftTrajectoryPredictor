40d740092db6421c28a1f6d369d00b6ad59c79ba
Robert <robert.pastor0691@orange.fr>
2015-06-07 20:32:47 +0200

    add test to aero Dynamics class

--- FlightDynamics/Home/BadaAircraftPerformance/BadaAeroDynamicsFile.py
+++ FlightDynamics/Home/BadaAircraftPerformance/BadaAeroDynamicsFile.py
@@ -26,7 +26,11 @@ Created on 6 mars 2015
  """
 this class is responsible for managing the AeroDynamics data provided for each aircraft by BADA
 '''
+
+import unittest
+
 from Home.BadaAircraftPerformance.BadaAircraftPerformanceFile import AircraftPerformance
+from Home.BadaAircraftPerformance.BadaAircraftDatabaseFile import BadaAircraftDatabase
 
 from Home.Environment.Atmosphere import Atmosphere
 from Home.Environment.Earth import Earth
@@ -101,5 +105,35 @@ class AeroDynamics(object):
         print self.className + ': Wing Area Surface= {0} Square-Meters'.format(self.WingAreaSurfaceSquareMeters)
         print self.className + ': stall speed= {0} knots'.format(self.VstallKcas)
         
+
+class Test_Class(unittest.TestCase):
+
+    def test_Class_One(self):
+            
+        print '================ test one ===================='
+        acBd = BadaAircraftDatabase()
+        assert acBd.read()
         
+        atmosphere = Atmosphere()
+        earth = Earth()
         
+        aircraftICAOcode = 'A320'
+        if ( acBd.aircraftExists(aircraftICAOcode) and
+             acBd.aircraftPerformanceFileExists(aircraftICAOcode)):
+            
+            print acBd.getAircraftFullName(aircraftICAOcode)
+            
+            aircraftPerformance = AircraftPerformance(acBd.getAircraftPerformanceFile(aircraftICAOcode))
+            aeroDynamics = AeroDynamics(aircraftPerformance, atmosphere, earth)
+            
+            print aeroDynamics
+            for phase in ['CR', 'IC', 'TO', 'AP', 'LD']:
+                print 'phase= {0} - Vstall CAS= {1} knots'.format(phase, aeroDynamics.getVstallKcas(phase))
+                
+            for phase in ['CR', 'IC', 'TO', 'AP', 'LD']:
+                print 'phase= {0} - Drag Coeff= {1}'.format(phase, aeroDynamics.getDragCoeff(phase))
+
+            print 'Wing Area Surface={0} Square-meters'.format(aeroDynamics.getWingAreaSurfaceSquareMeters())
+        
+if __name__ == '__main__':
+    unittest.main()
