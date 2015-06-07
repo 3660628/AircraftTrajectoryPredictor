0438e5f27b68a29a39167b355abe5a738550eb70
Robert <robert.pastor0691@orange.fr>
2015-06-07 08:40:13 +0200

    add test to Bada Engine class

--- FlightDynamics/Home/BadaAircraftPerformance/BadaEngineFile.py
+++ FlightDynamics/Home/BadaAircraftPerformance/BadaEngineFile.py
@@ -3,8 +3,6 @@ Created on 6 mars 2015
 
 @author: PASTOR Robert
 
-@author: PASTOR Robert
-
         Written By:
                 Robert PASTOR 
                 @Email: < robert [--DOT--] pastor0691 (--AT--) orange [--DOT--] fr >
@@ -26,6 +24,8 @@ Created on 6 mars 2015
         along with this program.  If not, see <http://www.gnu.org/licenses/>.
         
 '''
+import unittest
+from Home.BadaAircraftPerformance.BadaAircraftDatabaseFile import BadaAircraftDatabase
 
 from Home.BadaAircraftPerformance.BadaAircraftPerformanceFile import AircraftPerformance
 
@@ -165,5 +165,90 @@ class Engine(FuelConsumption):
         else:
             raise ValueError(self.className + ': computeNominalFuelFlow: not yet implemented')
         return fuelFlowKilograms
-    
 
+
+
+class Test_Class(unittest.TestCase):
+
+    def test_Class_One(self):
+            
+        print '================ test one ===================='
+        acBd = BadaAircraftDatabase()
+        assert acBd.read()
+        
+        aircraftICAOcode = 'A320'
+        if ( acBd.aircraftExists(aircraftICAOcode) and
+             acBd.aircraftPerformanceFileExists(aircraftICAOcode)):
+            
+            print acBd.getAircraftFullName(aircraftICAOcode)
+            
+            aircraftPerformance = AircraftPerformance(acBd.getAircraftPerformanceFile(aircraftICAOcode))
+            engine = Engine(aircraftPerformance)
+            
+            print 'engine is Jet= {0}'.format(engine.isJet())
+            print 'engine is Turbo Prop= {0}'.format(engine.isTurboProp())
+            print 'engine is Piston= {0}'.format(engine.isPiston())
+            self.assertTrue(engine.isJet(), 'A320 is a jet aircraft')
+
+
+    def test_Class_Two(self):
+        
+        print '================ test Two ===================='
+
+        acBd = BadaAircraftDatabase()
+        assert acBd.read()
+        
+        aircraftICAOcode = 'A320'
+        if ( acBd.aircraftExists(aircraftICAOcode) and
+             acBd.aircraftPerformanceFileExists(aircraftICAOcode)):
+            
+            print acBd.getAircraftFullName(aircraftICAOcode)
+            
+            aircraftPerformance = AircraftPerformance(acBd.getAircraftPerformanceFile(aircraftICAOcode))
+            engine = Engine(aircraftPerformance)
+            
+            print 'engine fuel consumption coeff= {0}'.format(engine.getFuelConsumptionCoeff())
+        
+        
+    def test_Class_Three(self):
+
+        print '================ test Three ===================='
+                
+        acBd = BadaAircraftDatabase()
+        assert acBd.read()
+        
+        aircraftICAOcode = 'A320'
+        if ( acBd.aircraftExists(aircraftICAOcode) and
+             acBd.aircraftPerformanceFileExists(aircraftICAOcode)):
+            
+            print acBd.getAircraftFullName(aircraftICAOcode)
+            
+            aircraftPerformance = AircraftPerformance(acBd.getAircraftPerformanceFile(aircraftICAOcode))
+            engine = Engine(aircraftPerformance)
+            
+            for index in range(0,5):
+                print index
+                print 'engine Max Climb Thrust coeff= {0}'.format(engine.getMaxClimbThrustCoeff(index))
+            
+    def test_Class_Four(self):
+
+        print '================ test Four ===================='
+
+        acBd = BadaAircraftDatabase()
+        assert acBd.read()
+        
+        aircraftICAOcode = 'A320'
+        if ( acBd.aircraftExists(aircraftICAOcode) and
+             acBd.aircraftPerformanceFileExists(aircraftICAOcode)):
+            
+            print acBd.getAircraftFullName(aircraftICAOcode)
+            
+            aircraftPerformance = AircraftPerformance(acBd.getAircraftPerformanceFile(aircraftICAOcode))
+            engine = Engine(aircraftPerformance)
+            
+            for index in range(0,5):
+                print index
+                print 'engine Descent Thrust Coeff= {0}'.format(engine.getDescentThrustCoeff(index))
+        
+if __name__ == '__main__':
+    unittest.main()
